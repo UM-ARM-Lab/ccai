Fix hessian calculation for GP. Add SGD with momentum as optimizer. Other changes

ccai/constrained_svgd_trajopt.py
@@ -5,7 +5,7 @@ from torch.profiler import profile, record_function, ProfilerActivity
 
 class ConstrainedSteinTrajOpt:
 
-    def __init__(self, problem, dt, alpha_C, alpha_J):
+    def __init__(self, problem, dt, alpha_C, alpha_J, momentum):
         self.dt = dt
 
         self.problem = problem
@@ -24,7 +24,7 @@ class ConstrainedSteinTrajOpt:
         self.alpha_C = alpha_C
         self.alpha_J = alpha_J
         self.iters = 500
-
+        self.momentum = momentum
         self.normxiJ = None
 
     def compute_update(self, xuz):
@@ -32,18 +32,17 @@ class ConstrainedSteinTrajOpt:
         d = self.dx + self.du
         xuz = xuz.detach()
         xuz.requires_grad = True
-        grad_J, K, grad_K, C, dC, hess_C = self.problem.eval(xuz)
+        grad_J, hess_J, K, grad_K, C, dC, hess_C = self.problem.eval(xuz)
 
         with torch.no_grad():
             # we try and invert the dC dCT, if it is singular then we use the psuedo-inverse
-
             eye = torch.eye(self.dg + self.dh).repeat(N, 1, 1).to(device=C.device)
             try:
                 dCdCT_inv = torch.linalg.solve(dC @ dC.permute(0, 2, 1), eye)
                 if torch.any(torch.isnan(dCdCT_inv)):
                     raise ValueError('nan in inverse')
             except Exception as e:
-                print(e)
+                #print(e)
                 # dCdCT_inv = torch.linalg.lstsq(dC @ dC.permute(0, 2, 1), eye).solution
                 dCdCT_inv = torch.linalg.pinv(dC @ dC.permute(0, 2, 1))
 
@@ -51,10 +50,10 @@ class ConstrainedSteinTrajOpt:
             projection = dCdCT_inv @ dC
             eye = torch.eye(d * self.T + self.dh, device=xuz.device, dtype=xuz.dtype).unsqueeze(0)
             projection = eye - dC.permute(0, 2, 1) @ projection
-
             # compute term for repelling towards constraint
             xi_C = dCdCT_inv @ C.unsqueeze(-1)
             xi_C = (dC.permute(0, 2, 1) @ xi_C).squeeze(-1)
+            #xi_C = (dC.permute(0, 2, 1) @ C.unsqueeze(-1)).squeeze(-1)
 
             # compute gradient for projection
             # now the second index (1) is the
@@ -95,10 +94,9 @@ class ConstrainedSteinTrajOpt:
                 matrix_K = projection.unsqueeze(0) @ K_extended @ projection.unsqueeze(1)
 
             else:
-                if self.use_fisher or self.use_hessian:
-                    Q_inv = 0.1 * torch.eye(d * self.T + self.dh, device=xuz.device)
-                    # Q_inv[:self.T*d, :self.T*d] = torch.linalg.pinv(Q)
-
+                if hess_J is not None:
+                    # Q_inv = 0.1 * torch.eye(d * self.T + self.dh, device=xuz.device)
+                    Q_inv = torch.linalg.pinv(hess_J)
                     PQ = projection @ Q_inv.unsqueeze(0)
                     PQP = PQ.unsqueeze(0) @ projection.unsqueeze(1)
                     first_term = torch.einsum('nmj, nmij->nmi', grad_K, PQP)
@@ -119,43 +117,31 @@ class ConstrainedSteinTrajOpt:
 
             # compute kernelized score
             kernelized_score = torch.sum(matrix_K @ -grad_J.reshape(N, 1, -1, 1), dim=0)
-            phi = self.gamma * kernelized_score.squeeze(-1) / N + grad_matrix_K / N
+            phi = self.gamma * kernelized_score.squeeze(-1) / N + grad_matrix_K / N  # maximize phi
+
             xi_J = -phi
 
-            # Normalize gradient
-            normxiC = torch.clamp(torch.linalg.norm(xi_C, dim=1, keepdim=True, ord=np.inf), min=1e-6)
-            normxiC = torch.min(0.9 * torch.ones_like(normxiC) / self.dt, normxiC)
-            xi_C = self.alpha_C * xi_C / normxiC
+            if False:
+                # Normalize gradient
+                normxiC = torch.clamp(torch.linalg.norm(xi_C, dim=1, keepdim=True, ord=np.inf), min=1e-6)
+                normxiC = torch.min(0.9 * torch.ones_like(normxiC) / self.dt, normxiC)
+                xi_C = self.alpha_C * xi_C / normxiC
 
-            if self.normxiJ is None:
-                self.normxiJ = torch.clamp(torch.linalg.norm(xi_J, dim=1, keepdim=True, ord=np.inf), min=1e-6)
-                xi_J = self.alpha_J * xi_J / self.normxiJ
-            else:
-                normxiJ = torch.clamp(torch.linalg.norm(xi_J, dim=1, keepdim=True, ord=np.inf), min=1e-6)
-                normxiJ = torch.max(normxiJ, self.normxiJ)
-                xi_J = self.alpha_J * xi_J / normxiJ
+                if self.normxiJ is None:
+                    self.normxiJ = torch.clamp(torch.linalg.norm(xi_J, dim=1, keepdim=True, ord=np.inf), min=1e-6)
+                    xi_J = self.alpha_J * xi_J / self.normxiJ
+                else:
+                    normxiJ = torch.clamp(torch.linalg.norm(xi_J, dim=1, keepdim=True, ord=np.inf), min=1e-6)
+                    normxiJ = torch.max(normxiJ, self.normxiJ)
+                    xi_J = self.alpha_J * xi_J / normxiJ
 
-        if torch.any(torch.isnan(xi_J)) or torch.any(torch.isnan(xi_C)):
-            # for debugging purposes
-            print(torch.any(torch.isnan(dCdCT_inv)))
-            print(torch.any(torch.isnan(dC)))
-            print(dC.shape)
-            print(torch.any(torch.isinf(dC)))
-            for d in dC:
-                print(d)
-                print(d.shape)
-                print(torch.linalg.matrix_rank(d))
-                print(torch.linalg.svdvals(d))
-            exit(0)
-        return (self.alpha_J * -xi_J - self.alpha_C * xi_C).detach()
+        return (self.alpha_J * xi_J + self.alpha_C * xi_C).detach()
 
     def _clamp_in_bounds(self, xuz):
         N = xuz.shape[0]
-        xuz = xuz.reshape(N, self.T, -1)
-        xuz[:, :, :self.dx + self.du] = torch.clamp(xuz[:, :, :self.dx + self.du],
-                                                    min=self.problem.x_min.reshape(1, 1, -1).to(device=xuz.device),
-                                                    max=self.problem.x_max.reshape(1, 1, -1).to(device=xuz.device))
-        return xuz.reshape(N, -1)
+        min_x = self.problem.x_min.reshape(1, 1, -1).repeat(1, self.problem.T, 1).reshape(1, -1)
+        max_x = self.problem.x_max.reshape(1, 1, -1).repeat(1, self.problem.T, 1).reshape(1, -1)
+        torch.clamp_(xuz, min=min_x.to(device=xuz.device), max=max_x.to(device=xuz.device))
 
     def solve(self, x0):
         self.normxiJ = None
@@ -168,6 +154,11 @@ class ConstrainedSteinTrajOpt:
         else:
             xuz = x0.clone().reshape(N, -1)
 
+        optim = torch.optim.SGD(params=[xuz], lr=self.dt, momentum=self.momentum,
+                                nesterov=True if self.momentum > 0 else False)
+
+        #optim = torch.optim.RMSprop(params=[xuz], lr=self.dt)
+
         # driving force useful for helping exploration -- currently unused
         T = self.iters
         C = 1
@@ -179,31 +170,37 @@ class ConstrainedSteinTrajOpt:
         for iter in range(T):
             s = time.time()
             if T > 100:
-                self.gamma = driving_force(iter+1)
+                self.gamma = driving_force(iter + 1)
             else:
                 self.gamma = 1
-            grad = self.compute_update(xuz)
-            new_xuz = xuz + self.dt * grad
 
+            optim.zero_grad()
+            old_xuz = xuz.clone()
+            grad = self.compute_update(xuz)
+            xuz.grad = grad
+            # print(torch.linalg.norm(grad))
+            torch.nn.utils.clip_grad_norm_(xuz, 10)
+            # new_xuz = xuz + self.dt * grad
+            optim.step()
             # clamp within bounds for simple bounds
             if self.problem.x_max is not None:
-                new_xuz = self._clamp_in_bounds(new_xuz)
-
-            old_C = self.problem.constraints(xuz)
-            new_C = self.problem.constraints(new_xuz)
+                self._clamp_in_bounds(xuz)
+            old_C = self.problem.constraints(old_xuz)
+            new_C = self.problem.constraints(xuz)
             improvement = -torch.max(new_C.abs(), dim=1).values + torch.max(old_C.abs(), dim=1).values
 
             # if any trajectory failed to improve then we replace it with a trajectory that has the lowest constraint
             # violation with some noise
             best_idx = torch.argmin(torch.max(new_C.abs(), dim=1).values)
-            best_traj = new_xuz[best_idx].unsqueeze(0)
-            new_xuz = torch.where(improvement.unsqueeze(-1) > -0.025,
-                                  new_xuz,
-                                  best_traj + 0.05 * torch.randn_like(best_traj)
+            best_traj = xuz[best_idx].unsqueeze(0)
+            xuz.data = torch.where(improvement.unsqueeze(-1) > -0.05,
+                                  xuz.data,
+                                  best_traj + 0.1 * torch.randn_like(best_traj)
                                   )
+
             if torch.all(torch.linalg.norm(grad, dim=-1) < 1e-3):
                 print(f'converged after {iter} iterations')
                 break
-            #print(time.time() - s)
-            xuz = new_xuz
+            # torch.cuda.synchronize()
+            # print(time.time() - s)
         return xuz.reshape(N, self.T, -1)[:, :, :self.dx + self.du]

ccai/kernels.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 
 
 def median(tensor):
@@ -10,18 +11,22 @@ def median(tensor):
     return (torch.cat((tensor, tensor_max)).median() + tensor.median()) / 2.
 
 
-def rbf_kernel(X, Xbar, M=None):
+def rbf_kernel(X, Xbar, Q=None):
     # X is N x d
+    n = X.shape[0]
+    X = X.reshape(n, -1)
+    Xbar = Xbar.reshape(n, -1)
     n, d = X.shape
     diff = X.unsqueeze(0) - Xbar.unsqueeze(1)
-    if M is not None:
-        scaled_diff = diff @ M.unsqueeze(0)
+    if Q is not None:
+        scaled_diff = diff @ Q.unsqueeze(0)
     else:
         scaled_diff = diff
 
     scaled_diff = (scaled_diff.reshape(-1, 1, d) @ diff.reshape(-1, d, 1)).reshape(n, n)
-    h = median(scaled_diff)
-    h = torch.sqrt(h / 2) + 1e-3
+    h = median(torch.sqrt(scaled_diff)) ** 2
+    h = h / (2 * np.log(n + 1)) + 1e-3
+
     # h = 0.1
     return torch.exp(-0.5 * scaled_diff / h)
 
@@ -33,11 +38,13 @@ def get_chunk(X, num_chunks, expected_chunk_size):
     return x
 
 
-def structured_rbf_kernel(X, Xbar):
+def structured_rbf_kernel(X, Xbar, Q=None):
     # X is N x T x d
     n, T, d = X.shape
     mod = T % 3
     num_chunks = T // 3
+    if Q is not None:
+        Xbar = (Q.reshape(1, T * d, T * d) @ Xbar.reshape(n, T * d, 1)).reshape(n, T, d)
 
     x1 = get_chunk(X, num_chunks, 3)
     x2 = get_chunk(X[:, 1:], num_chunks, 3)
@@ -56,8 +63,8 @@ def structured_rbf_kernel(X, Xbar):
     diff = x.unsqueeze(1) - xbar.unsqueeze(2)
 
     sq_diff = (diff.reshape(-1, 1, d) @ diff.reshape(-1, d, 1)).reshape(M, n, n)
-    h = median(sq_diff)
-    h = torch.sqrt(h / 2) + 1e-3
+    h = median(torch.sqrt(sq_diff)) ** 2
+    h = h / (2 * np.log(n + 1)) + 1e-3
     # h = 0.1
     return torch.exp(-0.5 * sq_diff / h).mean(dim=0)
 
@@ -92,6 +99,7 @@ class RBFKernel:
 
         K = sigma_sq * torch.exp(-0.5 * sq_diff / h)
         grad_K = - sigma_sq * diff * K.reshape(n, m, 1) / h
-        hess_K = sigma_sq * (diff.reshape(n, m, d, 1) @ diff.reshape(n, m, 1, d) - h) * K.reshape(n, m, 1, 1) / h
-
+        hess_K = diff.reshape(n, m, d, 1) @ diff.reshape(n, m, 1, d) * K.reshape(n, m, 1, 1) / h ** 2
+        hess_K = hess_K - torch.diag_embed(K.reshape(n, m, 1).repeat(1, 1, d)) / h
+        hess_K = hess_K * sigma_sq
         return K, grad_K, hess_K

ccai/quadrotor_env.py
@@ -10,7 +10,7 @@ class QuadrotorEnv:
     def __init__(self, surface_data_fname):
         self.env_dims = [-10, 10]
         self.state_dim = 12
-        self.dt = 0.1
+        self.dt = 0.05
         self.state = None
         data = np.load(surface_data_fname)
         self.surface_model = GPSurfaceModel(torch.from_numpy(data['xy']).to(dtype=torch.float32),
@@ -51,9 +51,13 @@ class QuadrotorEnv:
         start[3:5] = 0.05*(-np.pi + 2 * np.pi * np.random.rand(2))
 
         # Initial velocity zero for now - may change in future
-        # start[6:] = np.random.randn(6)
+        start[6:] = 0.0*np.random.randn(6)
+        print(start)
         # start[9:] *= 5
         self.state = start
+
+    def get_constraint_violation(self):
+        return self.state[2] - self._get_surface_h(self.state)
 
     def step(self, control):
         # Unroll state
@@ -104,6 +108,6 @@ class QuadrotorEnv:
                         self.surface_plotting_vars[2] - 1, alpha=0.5)
         ax.axes.set_zlim3d(bottom=-5, top=5)
         ax.scatter(self.state[0], self.state[1], self.state[2], s=100, c='g')
-        ax.scatter(0, 0, 0, s=100, c='k')
+        ax.scatter(4, 4, 0, s=100, c='k')
 
         return ax

examples/quadrotor_example.py
@@ -18,9 +18,11 @@ def cost(trajectory, goal):
     x = trajectory[:, :12]
     u = trajectory[:, 12:]
     Q = torch.eye(12, device=trajectory.device)
-    Q[3:] *= 0.0
-    P = 10 * torch.eye(12, device=trajectory.device)
-    R = 0.001*torch.eye(4, device=trajectory.device)
+    # high penalization on not rotation unstably
+    P = 100 * torch.eye(12, device=trajectory.device)
+    Q[2:, 2:] *= 0.001
+    P[2:, 2:] *= 0.05
+    R = 0.001 * torch.eye(4, device=trajectory.device)
 
     d2goal = x - goal.reshape(-1, 12)
 
@@ -29,7 +31,33 @@ def cost(trajectory, goal):
     terminal_state_cost = d2goal[-1].reshape(1, 12) @ P @ d2goal[-1].reshape(12, 1)
 
     cost = torch.sum(running_control_cost + running_state_cost, dim=0) + terminal_state_cost
-    return 0.01*cost.reshape(-1)
+
+    # Compute cost grad analytically
+    grad_running_state_cost = Q.reshape(1, 12, 12) @ d2goal.reshape(-1, 12, 1)
+    grad_running_control_cost = R.reshape(1, 4, 4) @ u.reshape(-1, 4, 1)
+    grad_terminal_cost = P @ d2goal[-1].reshape(12, 1)  # only on terminal
+    grad_terminal_cost = torch.cat((torch.zeros(T - 1, 12, 1, device=trajectory.device),
+                                    grad_terminal_cost.unsqueeze(0)), dim=0)
+
+    grad_cost = torch.cat((grad_running_state_cost + grad_terminal_cost, grad_running_control_cost), dim=1)
+    grad_cost = grad_cost.reshape(T * 16)
+
+    # compute hessian of cost analytically
+    running_state_hess = Q.reshape(1, 12, 12).repeat(T, 1, 1)
+    running_control_hess = R.reshape(1, 4, 4).repeat(T, 1, 1)
+    terminal_hess = torch.cat((torch.zeros(T - 1, 12, 12, device=trajectory.device), P.unsqueeze(0)), dim=0)
+
+    state_hess = running_state_hess + terminal_hess
+    hess_cost = torch.cat((
+        torch.cat((state_hess, torch.zeros(T, 4, 12, device=trajectory.device)), dim=1),
+        torch.cat((torch.zeros(T, 12, 4, device=trajectory.device), running_control_hess), dim=1)
+    ), dim=2)  # will be N x T x 16 x 16
+
+    # now we need to refactor hess to be (N x Td x Td)
+    hess_cost = torch.diag_embed(hess_cost.permute(1, 2, 0)).permute(2, 0, 3, 1).reshape(T * 16, T * 16)
+
+    alpha = 1e-1
+    return alpha * cost.flatten(), alpha * grad_cost, alpha * hess_cost
 
 
 class Quadrotor12DDynamics(torch.nn.Module):
@@ -86,7 +114,7 @@ class QuadrotorProblem:
         self.du = 4
         self.dz = 0
         self.dh = self.dz * T
-        self.dg = 12 * T + (T-1)
+        self.dg = 12 * T + T - 2
         #self.dg = 2 * T - 1
         data = np.load('surface_data.npz')
         # GP which models surface
@@ -94,7 +122,7 @@ class QuadrotorProblem:
                                          torch.from_numpy(data['z']).to(dtype=torch.float32, device=device))
 
         # gradient and hessian of dynamics
-        self.dynamics = Quadrotor12DDynamics(dt=0.1)
+        self.dynamics = Quadrotor12DDynamics(dt=0.05)
         self.dynamics_constraint = vmap(self._dynamics_constraint)
         self.grad_dynamics = vmap(jacrev(self._dynamics_constraint))
         self.hess_dynamics = vmap(hessian(self._dynamics_constraint))
@@ -102,8 +130,6 @@ class QuadrotorProblem:
         self.height_constraint = vmap(self._height_constraint)
 
         self.J = vmap(partial(cost, goal=goal))
-        self.dJ = vmap(jacrev(partial(cost, goal=goal)))
-        self.HJ = vmap(hessian(partial(cost, goal=goal)))
 
         self.start = start
         self.goal = goal
@@ -112,28 +138,31 @@ class QuadrotorProblem:
         # self.hess_g = vmap(hessian(self._combined_contraints))
 
         kernel = structured_rbf_kernel
+        # kernel = rbf_kernel
         self.K = kernel
         self.dK = jacrev(kernel, argnums=0)
         self.x_max = torch.ones(self.dx + self.du)
-        self.x_max[:3] = 5
-        self.x_max[3:6] = torch.pi
-        self.x_max[6:12] = 2
-        self.x_max[12:] = 10
-        self.x_max = 1000 * torch.ones(self.dx + self.du)
+        self.x_max[:3] = 6
+        self.x_max[3:5] = 0.4 * torch.pi
+        self.x_max[5] = 1000  # torch.pi
+        self.x_max[6:12] = 1000
+        self.x_max[12:] = 1000
+        # self.x_max = 1000 * torch.ones(self.dx + self.du)
         self.x_min = -self.x_max
 
     def _dynamics_constraint(self, trajectory):
         x = trajectory[:, :12]
-        u = trajectory[:, 12:]
+        u = trajectory[:-1, 12:]
 
-        current_x = torch.cat((self.start.reshape(1, 12), x[:-1]), dim=0)
-        next_x = x
+        #current_x = torch.cat((self.start.reshape(1, 12), x[:-1]), dim=0)
+        current_x = x[:-1]
+        next_x = x[1:]
 
+        #print(x.shape, u.shape, next_x.shape, current_x.shape)
         pred_next_x = self.dynamics(current_x, u)
-
-        diff = pred_next_x - next_x
+        #print(pred_next_x - next_x)
+        #return torch.sum((pred_next_x - next_x).abs(), dim=1).reshape(-1)
         return torch.reshape(pred_next_x - next_x, (-1,))
-        return torch.reshape(torch.sum((pred_next_x - next_x)**2, dim=1), (-1,))
 
     def _height_constraint(self, trajectory):
         T = trajectory.shape[0]
@@ -174,11 +203,17 @@ class QuadrotorProblem:
         hess_surf_constr = torch.diag_embed(torch.diag_embed(hess_surf_constr.permute(0, 2, 3, 1)))
         hess_surf_constr = hess_surf_constr.permute(0, 3, 4, 1, 5, 2).reshape(N, self.T, prob_dim, prob_dim)
 
-        g = torch.cat((dynamics_constr, surf_constr[:, 1:]), dim=1)
-        Dg = torch.cat((grad_dynamics_constr, grad_surf_constr[:, 1:]), dim=1)
-        DDg = torch.cat((hess_dynamics_constr, hess_surf_constr[:, 1:]), dim=1)
+        start_constraint = (trajectory[:, 0, :12] - self.start.reshape(1, 12)).reshape(N, 12)
+        grad_start_constraint = torch.zeros(N, 12, self.T, 16, device=trajectory.device)
+        grad_start_constraint[:, :12, 0, :12] = torch.eye(12, device=trajectory.device)
+        grad_start_constraint = grad_start_constraint.reshape(N, 12, -1)
+        hess_start_constraint = torch.zeros(N, 12, self.T*16, self.T*16, device=trajectory.device)
 
-        #print(surf_constr.abs().max(), dynamics_constr.abs().max())
+        g = torch.cat((dynamics_constr, start_constraint, surf_constr[:, 2:]), dim=1)
+        Dg = torch.cat((grad_dynamics_constr,  grad_start_constraint, grad_surf_constr[:, 2:]), dim=1)
+        DDg = torch.cat((hess_dynamics_constr, hess_start_constraint, hess_surf_constr[:, 2:]), dim=1)
+
+        #print(surf_constr[0])
         #return dynamics_constr, grad_dynamics_constr, hess_dynamics_constr
         return g, Dg, DDg
 
@@ -186,74 +221,90 @@ class QuadrotorProblem:
         N = augmented_trajectory.shape[0]
         trajectory = augmented_trajectory.reshape(N, self.T, -1)
 
-        grad_cost = self.dJ(trajectory).reshape(N, self.T, -1)
-        grad_cost = torch.cat((grad_cost,
-                               torch.zeros(N, self.T, self.dz, device=trajectory.device)
-                               ), dim=2).reshape(N, -1)
+        cost, grad_cost, hess_cost = self.J(trajectory)
+        hess_cost = hess_cost.mean(dim=0).detach()
+        #hess_cost = None
+        # grad_cost = torch.cat((grad_cost.reshape(),
+        #                       torch.zeros(N, self.T, self.dz, device=trajectory.device)
+        #                       ), dim=2).reshape(N, -1)
 
         # compute kernel and grad kernel
-        #Xk = trajectory.reshape(N, -1)
-        K = self.K(trajectory, trajectory)
-        grad_K = -self.dK(trajectory, trajectory).reshape(N, N, N, -1)
+        # Xk = trajectory.reshape(N, -1)
+        K = self.K(trajectory, trajectory, hess_cost)
+        grad_K = -self.dK(trajectory, trajectory, hess_cost).reshape(N, N, N, -1)
         grad_K = torch.einsum('nmmi->nmi', grad_K)
         grad_K = torch.cat((grad_K.reshape(N, N, self.T, self.dx + self.du),
                             torch.zeros(N, N, self.T, self.dz, device=trajectory.device)), dim=-1)
         grad_K = grad_K.reshape(N, N, -1)
-
         # Now we need to compute constraints and their first and second partial derivatives
         g, Dg, DDg = self.combined_constraints(trajectory)
 
         print(g.abs().max())
-        print(self.J(trajectory).reshape(-1))
-        return grad_cost.detach(), K.detach(), grad_K.detach(), g.detach(), Dg.detach(), DDg.detach()
+        print(cost.reshape(-1))
+        #print(g[0])
+        return grad_cost.detach(), hess_cost, K.detach(), grad_K.detach(), g.detach(), Dg.detach(), DDg.detach()
 
     def constraints(self, trajectory):
         return self.combined_constraints(trajectory)[0]
 
+
 if __name__ == "__main__":
-    N = 8
+    N = 4
     T = 12
     device = 'cuda:0'
     plt.ion()
     env = QuadrotorEnv('surface_data.npz')
     env.reset()
     env.render()
-    u = 0.1*torch.randn(N, T, 4, device=device)
+    u = 0.1 * torch.randn(N, T, 4, device=device)
     start = torch.from_numpy(env.state).to(dtype=torch.float32, device=device)
     goal = torch.zeros(12, device=device)
+    goal[:2] = 4
     problem = QuadrotorProblem(start, goal, T)
 
     # get initial x0
     x = [start.repeat(N, 1)]
-    for t in range(T):
+    for t in range(T-1):
         x.append(problem.dynamics(x[-1], u[:, t]))
 
-    trajectory = torch.cat((torch.stack(x[1:], dim=1), u), dim=2)
-    #trajectory = torch.randn_like(trajectory)
+    trajectory = torch.cat((torch.stack(x, dim=1), u), dim=2)
+    # trajectory = torch.randn_like(trajectory)
     alpha_C = 1
-    alpha_J = 0.5
-    solver_dt = 0.1
-    solver = ConstrainedSteinTrajOpt(problem, dt=solver_dt, alpha_J=alpha_J, alpha_C=alpha_C)
-    solver.iters = 500
+    alpha_J = 0.25
+    solver_dt = 0.5
+    momentum = 0.5
+
+    solver = ConstrainedSteinTrajOpt(problem, dt=solver_dt, alpha_J=alpha_J, alpha_C=alpha_C, momentum=momentum)
+    solver.iters = 1000
     trajectory = solver.solve(trajectory)
     num_steps = 100
-    online_iters = 25
-    for _ in range(num_steps):
+    online_iters = 20
+    for step in range(num_steps):
         start = torch.from_numpy(env.state).to(dtype=torch.float32, device=device)
+        trajectory[:, 0, :12] = start.repeat(N, 1)
         problem = QuadrotorProblem(start, goal, T)
-        solver = ConstrainedSteinTrajOpt(problem, dt=solver_dt, alpha_J=alpha_J, alpha_C=alpha_C)
+        solver = ConstrainedSteinTrajOpt(problem, dt=solver_dt, alpha_J=alpha_J, alpha_C=alpha_C, momentum=momentum)
         solver.iters = online_iters
         trajectory = solver.solve(trajectory)
 
-        costs = problem.J(trajectory)
+        costs, _, _ = problem.J(trajectory)
+        g = problem.constraints(trajectory)
+        lowest_violation_idx = torch.argmin(torch.max(g.abs(), dim=1).values)
         lowest_cost_idx = torch.argmin(costs)
 
         u = trajectory[lowest_cost_idx, 0, -4:].detach().cpu().numpy()
         env.step(u)
+
+        print(env.state[:3])
+        print(f'Constraint violation: {env.get_constraint_violation()}')
+        if np.linalg.norm(env.state[:2] - np.array([4, 4])) < 0.5:
+            print(f'Goal reached after {step} num steps')
+            break
+
         ax = env.render()
         for traj in trajectory:
             traj_np = traj.detach().cpu().numpy()
-            ax.plot(traj_np[:, 0], traj_np[:, 1], traj_np[:, 2])
+            ax.plot(traj_np[1:, 0], traj_np[1:, 1], traj_np[1:, 2])
         ax.view_init(60, -50)
         ax.axes.set_xlim3d(left=-5, right=5)
         ax.axes.set_ylim3d(bottom=-5, top=5)
@@ -261,16 +312,16 @@ if __name__ == "__main__":
 
         plt.draw()
         plt.pause(0.1)
-        #print('--')
-        #print(start)
-        #print(env.state)
-        #print(trajectory[lowest_cost_idx, 0, :12])
-        #exit(0)
+        # print('--')
+        # print(start)
+        # print(env.state)
+        # print(trajectory[lowest_cost_idx, 0, :12])
+        # exit(0)
         # display all planned trajectories
 
         # prepare trajectory for next time
         trajectory = torch.roll(trajectory, shifts=-1, dims=1)
-        trajectory[:, -1, -4:] = 0.1*torch.randn(N, 4, device=device)
-        trajectory[:, -1, :12] = problem.dynamics(trajectory[:, -2, :12], trajectory[:, -1, -4:])
+        trajectory[:, -1, -4:] = 0. * torch.randn(N, 4, device=device)
+        trajectory[:, -1, :12] = problem.dynamics(trajectory[:, -2, :12], trajectory[:, -2, -4:])
 
     plt.show(block=True)