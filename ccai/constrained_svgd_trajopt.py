Fix several bugs with gradient calculation. Perform data collection of turning and regrasping

ccai/constrained_svgd_trajopt.py
@@ -53,7 +53,6 @@ class ConstrainedSteinTrajOpt:
             dCdCT = dC @ dC.permute(0, 2, 1)
             dCdCT = dCdCT.to(dtype=self.dtype)
             A_bmm = lambda x: dCdCT @ x
-            print(torch.linalg.matrix_rank(dCdCT))
             #
             # damping_factor = 1e-1
             try:
@@ -176,6 +175,8 @@ class ConstrainedSteinTrajOpt:
     def resample(self, xuz):
         N = xuz.shape[0]
         xuz = xuz.to(dtype=torch.float32)
+        # need to preprocess
+        self.problem._preprocess(xuz)
         J = self.problem.get_cost(xuz.reshape(N, self.T, -1)[:, :, :self.dx + self.du])
         C, dC, _ = self.problem.combined_constraints(xuz.reshape(N, self.T, -1),
                                                      compute_grads=True,
@@ -254,7 +255,6 @@ class ConstrainedSteinTrajOpt:
         path = [xuz.data.clone()]
         self.gamma = self.max_gamma
         for iter in range(T):
-            print(iter)
             # reset slack variables
             # if self.problem.dz > 0:
             #    xu = xuz.reshape(N, self.T, -1)[:, :, :self.dx + self.du]