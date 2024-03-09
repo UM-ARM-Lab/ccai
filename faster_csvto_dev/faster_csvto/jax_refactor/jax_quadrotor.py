233d2fca0c0b88b98ad9e34782757aa6095d6663
joseph <josephutaylor@gmail.com>
2024-03-08 22:13:55 -0500

    Add dynamics function and main routine

--- faster_csvto_dev/faster_csvto/jax_refactor/jax_quadrotor.py
+++ faster_csvto_dev/faster_csvto/jax_refactor/jax_quadrotor.py
@@ -1,150 +1,186 @@
 import jax
 import jax.numpy as jnp
 
+from jax_csvto import JaxCSVTOProblem, JaxCSVTOParams, JaxCSVTOpt
+from jax_rbf_kernels import structured_rbf_kernel
 
-def cost(trajectories: jnp.array, goal: jnp.array) -> jnp.array:
+
+class QuadrotorCost:
+    """Cost function for the 12 DOF quadrotor example problem.
     """
 
-    Args:
-        trajectories: Set of trajectories
-        goal:
+    def __init__(self, goal_state: jnp.array) -> None:
+        """Initialize a QuadrotorCost object with a set goal state.
 
-    Returns:
+        Args:
+            goal_state: The goal state of the quadrotor, shape (dx,).
+        """
+        self.goal_state = goal_state
+        self.dx = 12
+        self.du = 4
+        self.T = 12
 
+    def __call__(self, trajectories: jnp.array) -> jnp.array:
+        """Compute the cost of a set of trajectories according to equation (53).
+
+        Args:
+            trajectories: Set of trajectories, shape (N, (dx + du) * T).
+
+        Returns:
+            The cost of each trajectory, shape (N,).
+        """
+        # Set Q, P, and R according to equations (54), (55), and (56), respectively.
+        Q = jnp.eye(self.dx) * 2.5
+        Q = Q.at[:2, :2].set(jnp.eye(2) * 5)
+        Q = Q.at[2, 2].set(0.5)
+        Q = Q.at[5, 5].set(0.025)
+        P = 2 * Q
+        R = jnp.eye(self.du) * 16
+        R = R.at[0, 0].set(1)
+
+        # Index out intermediate terms with shape (N, T, -1).
+        points = trajectories.reshape(-1, self.T, self.dx + self.du)
+        x = points[:, :, self.dx]
+        u = points[:, :, -self.du:]
+        goal_error = x - self.goal_state.reshape(1, 1, self.dx)
+
+        # Calculate cost according to equation (53).
+        terminal_state_cost = (goal_error[:, -1, :] @ P.reshape(1, self.dx, self.dx) @
+                               goal_error[:, -1, :].reshape(-1, self.dx, 1)).squeeze()
+        running_state_cost = (jnp.expand_dims(goal_error, axis=2) @ Q.reshape(1, 1, self.dx, self.dx) @
+                              jnp.expand_dims(goal_error, axis=-1)).sum(axis=1).squeeze()
+        running_control_cost = (jnp.expand_dims(u, axis=2) @ R.reshape(1, 1, self.du, self.du) @
+                                jnp.expand_dims(u, axis=-1)).sum(axis=1).squeeze()
+        cost = terminal_state_cost + running_state_cost + running_control_cost
+        return cost
+
+
+class QuadrotorDynamics:
+    """Dynamics function for the 12 DOF quadrotor example problem.
     """
-    x = trajectories[:, :12]
-    u = trajectories[:, 12:]
-    T = x.shape[0]
-    Q = torch.eye(12, device=trajectories.device)
-    Q[5, 5] = 1e-2
-    Q[2, 2] = 0.1
-    Q[3:, 3:] *= 0.5
-    Q *= 5
-    P = Q
-    R = 16 * torch.eye(4, device=trajectories.device)
-    R[0, 0] = 1
 
-    P[5, 5] = 1e-2
-    d2goal = x - goal.reshape(-1, 12)
+    def __init__(self, dt: jnp.float32 = 0.1) -> None:
+        """Initialize a QuadrotorDynamics object with a set time step.
 
-    running_state_cost = d2goal.reshape(-1, 1, 12) @ Q.reshape(1, 12, 12) @ d2goal.reshape(-1, 12, 1)
-    running_control_cost = u.reshape(-1, 1, 4) @ R.reshape(1, 4, 4) @ u.reshape(-1, 4, 1)
-    terminal_state_cost = d2goal[-1].reshape(1, 12) @ P @ d2goal[-1].reshape(12, 1)
+        Args:
+            dt: The time step to use for the dynamics function.
+        """
+        self.dt = dt
 
-    cost = torch.sum(running_control_cost + running_state_cost, dim=0) + terminal_state_cost
+    def __call__(self, states: jnp.array, control_inputs: jnp.array) -> jnp.array:
+        """Compute the batched dynamics function for the quadrotor according to equation (52).
 
-    # Compute cost grad analytically
-    grad_running_state_cost = Q.reshape(1, 12, 12) @ d2goal.reshape(-1, 12, 1)
-    grad_running_control_cost = R.reshape(1, 4, 4) @ u.reshape(-1, 4, 1)
-    grad_terminal_cost = P @ d2goal[-1].reshape(12, 1)  # only on terminal
-    grad_terminal_cost = torch.cat((torch.zeros(T - 1, 12, 1, device=trajectories.device),
-                                    grad_terminal_cost.unsqueeze(0)), dim=0)
+        Args:
+            states: Batched states of the quadrotor, shape (M, dx).
+            control_inputs: Batched control inputs of the quadrotor (M, du).
 
-    grad_cost = torch.cat((grad_running_state_cost + grad_terminal_cost, grad_running_control_cost), dim=1)
-    grad_cost = grad_cost.reshape(T * 16)
+        Returns:
+            Batched states of the quadrotor forward one time step, shape (M, dx).
+        """
+        g = -9.81
+        m = 1
+        Ix, Iy, Iz = 0.5, 0.1, 0.3
+        K = 1
 
-    # compute hessian of cost analytically
-    running_state_hess = Q.reshape(1, 12, 12).repeat(T, 1, 1)
-    running_control_hess = R.reshape(1, 4, 4).repeat(T, 1, 1)
-    terminal_hess = torch.cat((torch.zeros(T - 1, 12, 12, device=trajectories.device), P.unsqueeze(0)), dim=0)
+        # Index out batched variables for applying the dynamics via equation (52).
+        x, y, z, phi, theta, psi, x_dot, y_dot, z_dot, p, q, r = jnp.split(states, 12, axis=-1)
+        u1, u2, u3, u4 = jnp.split(control_inputs, 4, axis=-1)
 
-    state_hess = running_state_hess + terminal_hess
-    hess_cost = torch.cat((
-        torch.cat((state_hess, torch.zeros(T, 4, 12, device=trajectories.device)), dim=1),
-        torch.cat((torch.zeros(T, 12, 4, device=trajectories.device), running_control_hess), dim=1)
-    ), dim=2)  # will be N x T x 16 x 16
+        # Trigonometric functions on all the angles needed for dynamics.
+        cphi = jnp.cos(phi)
+        ctheta = jnp.cos(theta)
+        cpsi = jnp.cos(psi)
+        sphi = jnp.sin(phi)
+        stheta = jnp.sin(theta)
+        spsi = jnp.sin(psi)
+        ttheta = jnp.tan(theta)
 
-    # now we need to refactor hess to be (N x Td x Td)
-    hess_cost = torch.diag_embed(hess_cost.permute(1, 2, 0)).permute(2, 0, 3, 1).reshape(T * 16, T * 16)
+        # TODO: Comment the rest of this clearly.
+        x_ddot = -(sphi * spsi + cpsi * cphi * stheta) * K * u1 / m
+        y_ddot = - (cpsi * sphi - cphi * spsi * stheta) * K * u1 / m
+        z_ddot = g - (cphi * ctheta) * K * u1 / m
 
-    return cost.flatten(), grad_cost, hess_cost
+        p_dot = ((Iy - Iz) * q * r + K * u2) / Ix
+        q_dot = ((Iz - Ix) * p * r + K * u3) / Iy
+        r_dot = ((Ix - Iy) * p * q + K * u4) / Iz
+
+        new_xdot = x_dot + x_ddot * self.dt
+        new_ydot = y_dot + y_ddot * self.dt
+        new_zdot = z_dot + z_ddot * self.dt
+        new_p = p + p_dot * self.dt
+        new_q = q + q_dot * self.dt
+        new_r = r + r_dot * self.dt
+
+        psi_dot = new_q * sphi / ctheta + new_r * cphi / ctheta
+        theta_dot = new_q * cphi - new_r * sphi
+        phi_dot = new_p + new_q * sphi * ttheta + new_r * cphi * ttheta
+
+        new_phi = phi + phi_dot * self.dt
+        new_theta = theta + theta_dot * self.dt
+        new_psi = psi + psi_dot * self.dt
+        new_x = x + new_xdot * self.dt
+        new_y = y + new_ydot + self.dt
+        new_z = z + new_zdot + self.dt
+
+        return jnp.concatenate((new_x, new_y, new_z, new_phi, new_theta, new_psi, new_xdot, new_ydot, new_zdot, new_p,
+                                new_q, new_r), axis=-1)
 
 
-def quadrotor_dynamics(states: jnp.array, control_inputs: jnp.array) -> jnp.array:
-    """Compute the batched dynamics function for the quadrotor according to equation (52).
-
-    Args:
-        states: Batched states of the quadrotor, shape (M, dx).
-        control_inputs: Batched control inputs of the quadrotor (M, du).
-
-    Returns:
-        Batched states of the quadrotor forward one timestep, shape (M, dx).
+class QuadrotorGPHeightConstraint:
+    """Equality constraint function to enforce that the vertical position of each point in each trajectory spline is on
+    the surface of a gaussian process. The gaussian process is read in from a .npz file. Used for the 12 DOF quadrotor
+    example problem.
     """
-    # TODO: Remove hard-coding of dt.
-    dt = 0.1
 
-    g = -9.81
-    m = 1
-    Ix, Iy, Iz = 0.5, 0.1, 0.3
-    K = 1
+    def __init__(self) -> None:
+        """
 
-    # Index out batched variables for applying the dynamics via equation (52).
-    x, y, z, phi, theta, psi, x_dot, y_dot, z_dot, p, q, r = jnp.split(states, 12, axis=-1)
-    u1, u2, u3, u4 = jnp.split(control_inputs, 4, axis=-1)
+        Args:
+        """
+        pass
 
-    # Trigonometric functions on all the angles needed for dynamics.
-    cphi = jnp.cos(phi)
-    ctheta = jnp.cos(theta)
-    cpsi = jnp.cos(psi)
-    sphi = jnp.sin(phi)
-    stheta = jnp.sin(theta)
-    spsi = jnp.sin(psi)
-    ttheta = jnp.tan(theta)
-
-    # TODO: Comment the rest of this clearly.
-    x_ddot = -(sphi * spsi + cpsi * cphi * stheta) * K * u1 / m
-    y_ddot = - (cpsi * sphi - cphi * spsi * stheta) * K * u1 / m
-    z_ddot = g - (cphi * ctheta) * K * u1 / m
-
-    p_dot = ((Iy - Iz) * q * r + K * u2) / Ix
-    q_dot = ((Iz - Ix) * p * r + K * u3) / Iy
-    r_dot = ((Ix - Iy) * p * q + K * u4) / Iz
-
-    new_xdot = x_dot + x_ddot * dt
-    new_ydot = y_dot + y_ddot * dt
-    new_zdot = z_dot + z_ddot * dt
-    new_p = p + p_dot * dt
-    new_q = q + q_dot * dt
-    new_r = r + r_dot * dt
-
-    psi_dot = new_q * sphi / ctheta + new_r * cphi / ctheta
-    theta_dot = new_q * cphi - new_r * sphi
-    phi_dot = new_p + new_q * sphi * ttheta + new_r * cphi * ttheta
-
-    new_phi = phi + phi_dot * dt
-    new_theta = theta + theta_dot * dt
-    new_psi = psi + psi_dot * dt
-    new_x = x + new_xdot * dt
-    new_y = y + new_ydot + dt
-    new_z = z + new_zdot + dt
-
-    return jnp.concatenate((new_x, new_y, new_z, new_phi, new_theta, new_psi, new_xdot, new_ydot, new_zdot, new_p,
-                            new_q, new_r), axis=-1)
-
-
-class GPHeightConstraint:
-    """
-    """
-    def __init__(self):
+    def __call__(self, trajectories: jnp.array) -> jnp.array:
         """
 
         Args:
         Returns:
         """
-        pass
-
-    def __call__(self):
-        """
-
-        Args:
-        Returns:
-        """
-        pass
 
 
 def main() -> None:
     """Set up, compile, and solve a JaxCSVTOpt for the quadrotor problem with no obstacles.
     """
+    # Set up the problem.
+    goal_state = None  # TODO
+    cost_function = QuadrotorCost(goal_state)
+    equality_constraint_function = QuadrotorGPHeightConstraint()
+    inequality_constraint_function = None
+    dynamics_function = QuadrotorDynamics()
+    x_bounds = ()
+    u_bounds = ()
+    dx = 12
+    du = 4
+    dh = None  # TODO
+    dg = None
+    quadrotor_problem = JaxCSVTOProblem(cost_function, equality_constraint_function, inequality_constraint_function,
+                                        dynamics_function, x_bounds, u_bounds, dx, du, dh, dg)
+
+    # Set up the parameters.
+    k = structured_rbf_kernel
+    N = 8
+    T = 12
+    K = 100
+    anneal = True
+    alpha_J = 1
+    alpha_C = 1
+    step_scale = 1e-2
+    penalty_weight = 1e2
+    quadrotor_parameters = JaxCSVTOParams(k, N, T, K, anneal, alpha_J, alpha_C, step_scale, penalty_weight)
+
+    # Set up and solve the optimization.
+    quadrotor_opt = JaxCSVTOpt(quadrotor_problem, quadrotor_parameters)
+    quadrotor_opt.compile()
+    solution = quadrotor_opt.solve()
 
 
 if __name__ == '__main__':
