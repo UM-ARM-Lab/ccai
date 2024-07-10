23ce590141acfe52606d4a07feea3d7c1a007992
Fan Yang <fanyangr@umich.edu>
2024-07-10 19:16:18 -0400

    temp

--- backup/allegro_valve.py
+++ backup/allegro_valve.py
@@ -52,22 +52,6 @@ env = AllegroValveTurningEnv(1, control_mode='joint_impedance', use_cartesian_co
 world_trans = env.world_trans
 
 
-def partial_to_full_state(partial):
-    """
-    :params partial: B x 8 joint configurations for index and thumb
-    :return full: B x 16 joint configuration for full hand
-
-    # assume that default is zeros, but could change
-    """
-    index, thumb = torch.chunk(partial, chunks=2, dim=-1)
-    full = torch.cat((
-        index,
-        torch.zeros_like(index),
-        torch.zeros_like(index),
-        thumb
-    ), dim=-1)
-    return full
-
 
 class AllegroValveProblem(ConstrainedSVGDProblem):
 
