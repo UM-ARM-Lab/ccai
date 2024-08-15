b086c0d02c2d540824dba8206352e7513e5e63c4
Abhinav <abhin@umich.edu>
2024-08-15 16:17:46 -0400

    Update for data generation

--- examples/allegro_contact.py
+++ examples/allegro_contact.py
@@ -711,7 +711,8 @@ class AllegroRegraspProblem(AllegroObjectProblem):
     def _contact_avoidance(self, xu, finger_name, compute_grads=True, compute_hess=False):
         h, grad_h, hess_h = self._contact_constraints(xu, finger_name, compute_grads, compute_hess, terminal=False)
         eps = torch.zeros_like(h)
-        eps[:, :-1] = 5e-3
+        # eps[:, :-1] = 5e-3
+        eps[:, :-1] = 1e-2
         h = -h + eps
         if grad_h is not None:
             grad_h = -grad_h
