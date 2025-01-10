Testing spline to fix derivatives

ccai/models/cnf/cnf.py
@@ -90,7 +90,7 @@ def set_cnf_options(solver, model):
             if solver in ['fixed_adams', 'explicit_adams']:
                 module.solver_options['max_order'] = 4
             if solver == 'rk4':
-                module.solver_options['step_size'] = .1
+                module.solver_options['step_size'] = .003
 
             # Set the test settings
             module.test_solver = solver
@@ -205,21 +205,39 @@ class TrajectoryCNF(nn.Module):
 
     def masked_grad(self, t, xdx, context=None):
         # print(f'step {self.step_id}')
+        if t == 1:
+            return torch.zeros_like(xdx)
         self.step_id += 1
+        # print(t)
+        x_arange = torch.linspace(0, 1, self.traj.shape[1], device=self.traj.device).expand(self.traj.shape[0], -1)
+        # t_mask = x_arange > t
+        # x_arange[:, 0] = t
+        # x_arange = x_arange[t_mask]
+        # Concat t to x_arange front
+        # x_arange = torch.cat((t.reshape(1), x_arange)).unsqueeze(0)
+        # self.traj[:, 0] = xdx[:, :self.xu_dim]
+        # traj = torch.cat((xdx.unsqueeze(1), self.traj[t_mask].unsqueeze(0)), dim=1)
 
-        x = xdx[..., :self.xu_dim]
-        dx = xdx[..., self.xu_dim:]
-        dx, ddx, _, _ = self.model.compiled_conditional_test(t, x, dx, context)
-
-        # if hasattr(self, 'label_norm_a'):
-        #     ddx = self.label_norm_a.running_mean + ddx * self.label_norm_a.running_var.sqrt()
-
+        traj = self.traj
+        _, dx, ddx = self.interp(x_arange, traj, t.reshape(-1, 1))
         ds_ret = torch.zeros_like(xdx)
-        if dx is None:
-            ds_ret[..., :self.xu_dim] = xdx[..., self.xu_dim:]
-        else:
-            ds_ret[..., :self.xu_dim] = dx
-        ds_ret[..., self.xu_dim:] = ddx
+        # ds_ret[..., :self.xu_dim] = dx*12
+        ds_ret[..., :self.xu_dim] = xdx[..., self.xu_dim:]
+        ds_ret[..., self.xu_dim:] = ddx# * 12#*12
+
+        # x = xdx[..., :self.xu_dim]
+        # dx = xdx[..., self.xu_dim:]
+        # dx, ddx, _, _ = self.model.compiled_conditional_test(t, x, dx, context)
+
+        # # if hasattr(self, 'label_norm_a'):
+        # #     ddx = self.label_norm_a.running_mean + ddx * self.label_norm_a.running_var.sqrt()
+
+        # ds_ret = torch.zeros_like(xdx)
+        # if dx is None:
+        #     ds_ret[..., :self.xu_dim] = xdx[..., self.xu_dim:]
+        # else:
+        #     ds_ret[..., :self.xu_dim] = dx
+        # ds_ret[..., self.xu_dim:] = ddx
         
         return ds_ret
 
@@ -496,14 +514,16 @@ class TrajectoryCNF(nn.Module):
 
         # Predict first derivative
         t_0 = torch.zeros(N, 1, device=self.noise.device) + start_time
-        _, vt, _ = self.model.compiled_conditional_test_fwd(t_0, self.noise, context)
-
-        vt *= 1.
+        # _, vt, _ = self.model.compiled_conditional_test_fwd(t_0, self.noise, context)
+        x_arange = torch.linspace(0, 1, self.traj.shape[1], device=self.traj.device).expand(self.traj.shape[0], -1)
+        self.traj[:, 0] = self.noise
+        _, vt, _ = self.interp(x_arange, self.traj, t_0)
+        # vt *= 1.
         # if hasattr(self, 'label_norm_v'):
         #     vt = self.label_norm_v.running_mean + vt * self.label_norm_v.running_var.sqrt()
         # self.noise = torch.cat((self.noise, torch.zeros_like(self.noise)), dim=-1)
         
-        self.noise = torch.cat((self.noise, vt), dim=-1)
+        self.noise = torch.cat((self.noise, vt*12), dim=-1)
         # manually set the conditions
         if condition is not None:
             self.condition = condition
@@ -537,14 +557,15 @@ class TrajectoryCNF(nn.Module):
         trajectories = trajectories[..., :self.xu_dim].reshape(N, -1, self.xu_dim)
         self.FM.sigma = sigma_save
 
-        turn_problem = self.problem_dict[(1, 1, 1)]
-        turn_problem.start = trajectories[0, 0, :15]
+        # turn_problem = self.problem_dict[(1, 1, 1)]
+        # turn_problem.start = trajectories[0, 0, :15]
 
-        turn_problem._preprocess(trajectories[:, 1:2])
+        # turn_problem._preprocess(trajectories[:, 1:2])
 
-        u_hat = turn_problem.solve_for_u_hat(trajectories[:, 1:2])
+        # u_hat = turn_problem.solve_for_u_hat(trajectories[:, 1:2])
 
-        trajectories[:, 1, 15:27] = u_hat
+        # trajectories[:, 1, 15:27] = u_hat
+        # trajectories, _, _ = self.interp(x_arange, self.traj, x_arange)
 
         return trajectories, log_prob.reshape(N, -1).sum(dim=1)
 

examples/allegro_screwdriver.py
@@ -532,6 +532,16 @@ def do_trial(env, params, fpath, sim_viz_env=None, ros_copy_node=None, inits_noi
                                                vae=vae)
         # try:
         trajectory_sampler.load_state_dict(torch.load(f'{CCAI_PATH}/{model_path}', map_location=torch.device(params['device'])), strict=True)
+        traj = torch.load(f'{CCAI_PATH}/examples/sanity_check_turn_traj.pt')
+        traj = torch.tensor(traj)
+        orig_yaw = traj[0, 14].item()
+        traj[:, 14] -= orig_yaw
+        traj = (traj - trajectory_sampler.model.x_mean) / trajectory_sampler.model.x_std
+        traj = traj.unsqueeze(0)
+        traj = traj.to(params['device'])
+
+        trajectory_sampler.model.traj = traj
+        
         # except:
         #     print('failed to load model')
         trajectory_sampler.to(device=params['device'])
@@ -1216,6 +1226,7 @@ def do_trial(env, params, fpath, sim_viz_env=None, ros_copy_node=None, inits_noi
             del actual_trajectory_save
             continue
         if stage == 0:
+            continue
             contact = 'pregrasp'
             start = env.get_state()['q'].reshape(4 * num_fingers + 4).to(device=params['device'])
             a = time.perf_counter()
@@ -1496,7 +1507,7 @@ if __name__ == "__main__":
         now = datetime.datetime.now().strftime("%m.%d.%y:%I:%M:%S")
     else:
         now = ''
-    start_ind = 5# if config['experiment_name'] == 'allegro_screwdriver_csvto_diff_sine_cosine_eps_.015_2.5_damping_pi_6' else 0
+    start_ind = 0# if config['experiment_name'] == 'allegro_screwdriver_csvto_diff_sine_cosine_eps_.015_2.5_damping_pi_6' else 0
     for i in tqdm(range(start_ind, config['num_trials'])):
     # for i in tqdm([1, 2, 4, 7]):
         if config['mode'] != 'hardware':
@@ -1506,7 +1517,19 @@ if __name__ == "__main__":
         goal = torch.tensor([0, 0, float(config['goal'])])
         # goal = goal + 0.025 * torch.randn(1) + 0.2
         for controller in config['controllers'].keys():
-            env.reset()
+            # env.reset()
+            traj = torch.load(f'{CCAI_PATH}/examples/sanity_check_turn_traj.pt')
+            traj = torch.tensor(traj).to(device=config['sim_device'])
+            traj[..., 14] -= traj[0, 14]
+            start_state = traj[0]
+            env.reset(
+                dof_pos=torch.cat(
+                                    [
+                                        start_state[:8],
+                                        torch.tensor([0, 0, 0, 0,]),
+                                        start_state[8:15], 
+                                        torch.tensor([0.]) ], dim=0).reshape(1, -1)
+            )
             fpath = pathlib.Path(f'{CCAI_PATH}/data/experiments/{config["experiment_name"]}.{now}/{controller}/trial_{i + 1}')
             if config['mode'] != 'hardware':
                 pathlib.Path.mkdir(fpath, parents=True, exist_ok=True)