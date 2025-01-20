10ddb4f34ebf9bd30938d2db6fb2f67990703e67
Adam Hung <adamhung@umich.edu>
2025-01-20 16:42:11 -0500

    check python and torch

--- _value_function/cuda_version.py
+++ _value_function/cuda_version.py
@@ -1,2 +1,10 @@
 import torch
+import sys
+
 print(torch.version.cuda)   # e.g. '11.7'
+
+# Print Python version
+print(f"Python version: {sys.version}")
+
+# Print PyTorch version
+print(f"PyTorch version: {torch.__version__}")
